6987f114e0b5a888e745e417da585a4e1c0a601a
gunthernic <gunthernic@eisti.eu>
2016-12-10 16:26:59 +0100

    t

--- src/main/java/com/example/appengine/gettingstartedjava/helloworld/Upload.java
+++ src/main/java/com/example/appengine/gettingstartedjava/helloworld/Upload.java
@@ -1,32 +1,33 @@
-package com.example.appengine.gettingstartedjava.helloworld;
+package  com.example.appengine.gettingstartedjava.helloworld;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import  com.google.appengine.api.blobstore.BlobKey;
+import  com.google.appengine.api.blobstore.BlobstoreService;
+import  com.google.appengine.api.blobstore.BlobstoreServiceFactory;
 
-import com.google.appengine.api.blobstore.BlobKey;
-import com.google.appengine.api.blobstore.BlobstoreService;
-import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+import  java.io.IOException;
+import  java.util.List;
+import  java.util.Map;
 
-public class Upload extends HttpServlet {
-    private BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+import  javax.servlet.ServletException;
+import  javax.servlet.http.HttpServlet;
+import  javax.servlet.http.HttpServletRequest;
+import  javax.servlet.http.HttpServletResponse;
 
-    @Override
-    public void doPost(HttpServletRequest req, HttpServletResponse res)
-        throws ServletException, IOException {
+public  class  Upload  extends  HttpServlet  {
+  private  BlobstoreService  blobstoreService  =  BlobstoreServiceFactory.getBlobstoreService();
 
-        Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(req);
-        List<BlobKey> blobKeys = blobs.get("myFile");
+  @Override
+  public  void  doPost(HttpServletRequest  req,  HttpServletResponse  res)
+    throws  ServletException,  IOException  {
 
-        if (blobKeys == null || blobKeys.isEmpty()) {
-            res.sendRedirect("/");
-        } else {
-            res.sendRedirect("/serve?blob-key=" + blobKeys.get(0).getKeyString());
-        }
+    Map<String,  List<BlobKey>>  blobs  =  blobstoreService.getUploads(req);
+    List<BlobKey>  blobKeys  =  blobs.get("myFile");
+
+    if  (blobKeys  ==  null  ||  blobKeys.isEmpty())  {
+      res.sendRedirect("/");
+    }  else  {
+      res.sendRedirect("/serve?blob-key="  +  blobKeys.get(0).getKeyString());
     }
+  }
 }
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,21 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java"%>
+<%@ page import="java.util.List"%>
+<%@ page import="com.google.appengine.api.blobstore.*"%>
+
+<%
+    BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+%>
+
+
+<html>
+    <head>
+        <title>Upload Test</title>
+    </head>
+    <body>
+        <form action="<%= blobstoreService.createUploadUrl("/upload") %>" method="post" enctype="multipart/form-data">
+            <input type="text" name="foo">
+            <input type="file" name="myFile">
+            <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
